72651b956332ba7c643213356858873152977826
Max <max@max.ru>
2018-07-31 18:32:32 +0300

    -адаптива для мобилки. Пока без меню

---
+++ scripts/burger-x.js
@@ -0,0 +1,16 @@
+(function() {
+
+  "use strict";
+
+  var toggle = document.getElementById("1");
+
+    toggleHandler(toggle);
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
