c9034ce76ac2469ba695ef09f9bb1d0303e06e17
Max <max@max.ru>
2018-08-01 15:16:42 +0300

    -прикрутил выездное меню через js

--- scripts/burger-x.js
+++ scripts/burger-x.js
@@ -2,15 +2,27 @@
 
   "use strict";
 
-  var toggle = document.getElementById("1");
+  // BURGER
+  var burger_button = document.getElementById("burger");
+  var menu = document.getElementsByClassName('menu')[0];
+  var page = document.getElementsByClassName('page')[0];
+  // console.log("menu", menu );
+  //console.log("page",page);
 
-    toggleHandler(toggle);
+	burger_button.addEventListener( "click", function(e) {
+	  
+	  e.preventDefault();
 
-  function toggleHandler(toggle) {
-    toggle.addEventListener( "click", function(e) {
-      e.preventDefault();
-      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-    });
-  }
+	  if(burger_button.classList.contains("burger_active") === true) { // is close
+	  	burger_button.classList.remove("burger_active");
+	  	menu.classList.remove('menu_opened');
+	  	page.classList.remove('page_close');
+  	}else{// is opened
+  		burger_button.classList.add("burger_active");
+  		menu.classList.add('menu_opened');
+  		page.classList.add('page_close');
+  	}
 
-})();
+	});
+
+}) ();
